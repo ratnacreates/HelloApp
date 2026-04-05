4af79a0fe5fd9dcc4d7ca7b42789e67ff121ce9d
ratnacreates <ratnapriya120512@gmail.com>
2026-04-05 21:41:35 +0530

    UC5: Enhanced for loop implementation

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name = "World";
-
-        if (args.length > 0) {
-            name = String.join(", ", args);
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
         }
-
-        System.out.println("Hello, " + name + "!");
     }
 }
