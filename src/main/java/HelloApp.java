63adae62c77c8c74a55baaa7783836ae7bc96dae
ratnacreates <ratnapriya120512@gmail.com>
2026-04-05 21:47:21 +0530

    UC6: substring method implementation

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -4,9 +4,16 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+            StringBuilder nameBuilder = new StringBuilder();
+
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                nameBuilder.append(name).append(", ");
             }
+
+            // Remove last ", "
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
